b20b5efc67a5bc3a85d6d1d86342a93bdfc60e45
Sam <s-peeters@hotmail.com>
2022-08-16 15:24:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmatphoneWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
